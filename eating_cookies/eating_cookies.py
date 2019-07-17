da0b6585a320d74cd27424c80844d6f023f2e147
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-07-17 18:10:16 -0500

    figuing out steps

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -26,12 +26,15 @@ n=5 cookies
 # 4 = 1 + 1 + 2 
 # 4 = 1 + 1 + 1 + 1
 
-def eating_cookies(n, cache=None):
-  pass
+#def eating_cookies(n):
+  #results
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+# def eating_cookies(n, cache=None):
+#   pass
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
